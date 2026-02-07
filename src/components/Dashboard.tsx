bceeaf46193e1f9476bd08f14acf3b8817c5be5f
phelipepunkstar-max <phelipepunkstar@gmail.com>
2026-02-07 13:21:40 -0400

    Create Analysis Results Display Component

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>Analysis Results</h1>
+            {/* Display analysis results here */}
+        </div>
+    );
+};
+
+export default Dashboard;
